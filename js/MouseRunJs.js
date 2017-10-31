fc3c1dd3d7d486d72ce1dbb8fcc7ca557921ae35
ethan <zz511627807@163.com>
2017-10-31 16:08:52 +0800

    modify

---
+++ js/MouseRunJs.js
@@ -0,0 +1,20 @@
+/**
+ * Created by macofethan on 17/10/31.
+ */
+
+
+$(document).ready(function () {
+
+    var top = 4;
+
+    setInterval(function () {
+
+        top+=1;
+
+        if (top == 25){top = 4}
+
+        $('.wheel').css('top', top)
+
+    }, 50)
+})
+
