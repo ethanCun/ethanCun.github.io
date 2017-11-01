161ea5e6473c1989aa459ccb7fafa3e766d5bd20
ethan <zz511627807@163.com>
2017-11-01 10:56:15 +0800

    modify

--- js/MouseRunJs.js
+++ js/MouseRunJs.js
@@ -5,6 +5,7 @@
 
 $(document).ready(function () {
 
+    //鼠标滚动动画
     var top = 4;
 
     setInterval(function () {
@@ -16,6 +17,43 @@ $(document).ready(function () {
         $('.wheel').css('top', top)
 
     }, 50)
+
+    //导航栏渐变
+    $(document).ready(function () {
+
+
+        $(window).scroll(function () {
+
+            if($(this).scrollTop()>100) {
+                $('.banner').css('backgroundColor', '#4ecdc4')
+            }else {
+                $('.banner').css('backgroundColor', 'transparent')
+            }
+        })
+    })
+    
+    
+//    购物车动画
+    $(window).scroll(function () {
+
+        console.log('位置:'+$(this).scrollTop());
+
+        if($(this).scrollTop()>250){
+
+            $('.shoppingCar_a').show(500);
+        }else {
+
+            $('.shoppingCar_a').hide(500);
+        }
+
+        if($(this).scrollTop()>900){
+
+            $('.ReactNativeConfigContentAero_a').show(500);
+        }else {
+
+            $('.ReactNativeConfigContentAero_a').hide(500);
+        }
+    })
 
 })
 
