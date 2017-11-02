ecd29a85884ab96495c45bc9b7ee1f1734ebbc47
ethan <zz511627807@163.com>
2017-11-02 15:50:28 +0800

    modify

---
+++ js/iOS/iOSArticleList/iOSArticleList.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    $('.first').click(function () {
+
+        $('.ul1').show();
+        $('.ul2').hide();
+
+    });
+
+    $('.second').click(function () {
+
+        $('.ul1').hide();
+        $('.ul2').show();
+    })
+})/**
+ * Created by macofethan on 17/11/2.
+ */
